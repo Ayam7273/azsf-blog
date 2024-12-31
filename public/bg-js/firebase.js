a734cdc63d18f9f5e12f0ca7817d3c1c82bc5e89
Abdulbasit Adio <abdulbasitadio@gmail.com>
2024-12-31 16:43:54 +0100

    1st commit

---
+++ public/bg-js/firebase.js
@@ -0,0 +1,14 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyBEzTzy5Eybz7tZgA0_mkbgFsZaxz5eeDA",
+    authDomain: "al-ihsan-zakat-n-sadaqah.firebaseapp.com",
+    projectId: "al-ihsan-zakat-n-sadaqah",
+    storageBucket: "al-ihsan-zakat-n-sadaqah.firebasestorage.app",
+    messagingSenderId: "477652997521",
+    appId: "1:477652997521:web:3115d375af0af809abf630"
+
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
---
+++ public/bg-js/news.js
@@ -0,0 +1,61 @@
+const blogSection = document.querySelector('.blogs-section');
+
+db.collection("blogs").get().then((blogs) => {
+    blogs.forEach(blog => {
+        if(blog.id != decodeURI(location.pathname.split("/").pop())){
+            createBlog(blog);
+        }
+    })
+})
+
+const createBlog = (blog) => {
+    let data = blog.data();
+    blogSection.innerHTML += `
+    <div class="blog-card">
+        <img src="${data.bannerImage}" class="blog-image" alt="">
+        <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
+        <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
+        <a href="/${blog.id}" class="btn dark">read</a>
+    </div>
+    `;
+}
+
+
+// 0. Preloader
+window.addEventListener("load", function() {
+    var preloader = document.getElementById('preloader');
+    preloader.style.display = 'none'; // Hide preloader
+});
+  
+// 1. Hamburger menu
+const nav = document.querySelector(".nav"),
+navOpenBtn = document.querySelector(".navOpenBtn"),
+navCloseBtn = document.querySelector(".navCloseBtn");
+  
+navOpenBtn.addEventListener("click", () => {
+    nav.classList.add("openNav");
+    nav.classList.remove("openSearch");
+    searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+    nav.classList.remove("openNav");
+});
+
+// 2. NavBar Drop down
+document.addEventListener("DOMContentLoaded", () => {
+    const dropdown = document.querySelector(".dropdown");
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+    const dropdownLink = dropdown.querySelector("a");
+  
+    // Toggle dropdown visibility on click
+    dropdownLink.addEventListener("click", (e) => {
+        dropdownMenu.classList.toggle("show");
+    });
+  
+    // Close dropdown if clicking outside
+    document.addEventListener("click", (e) => {
+        if (!dropdown.contains(e.target)) {
+            dropdownMenu.classList.remove("show");
+        }
+    });
+  });
